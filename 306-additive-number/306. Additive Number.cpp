837f9afa2943627e7178c907f40203bc27fa38b0
Amarjeet Mohanty <111443291+AmarjeetMohanty@users.noreply.github.com>
2023-12-15 02:39:33 +0530

    Runtime: 3 ms (faster than 28.23%), Memory: 6.6 MB (less than 45.05%) - GitSync

---
+++ 306-additive-number/306. Additive Number.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isAdditiveNumber(string num) {
+        string first , sec , third;
+        int n = num.size();
+
+        if(n <= 2){
+            return false;
+        }
+        else if( n == 3){
+            if (num[0] - '0' + num[1] - '0' == num[2] - '0'){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            for (int i = 1; i <= n / 2; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    string first = num.substr(0, i);
+                    string sec = num.substr(i, j - i);
+
+                    if ((first.size() > 1 && first[0] == '0') || (sec.size() > 1 && sec[0] == '0'))
+                        continue;
+
+                    long long num1 = stoll(first);
+                    long long num2 = stoll(sec);
+
+                    int k = j;
+                    while (k < n) {
+                        num2 = num1 + num2;
+                        first = to_string(num2);
+
+                        if (num.compare(k, first.size(), first) != 0)
+                            break;
+
+                        k += first.size();
+                        num1 = stoll(sec);
+                        sec = first;
+
+                        if (k == n)
+                            return true;
+                    }
+                }
+            }
+            
+            return false;
+        }
+    }
+};
